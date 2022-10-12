afea0cc3cf46cdfb368bfafe2d4ebe030f0270d8
Junbo-chen <junbochen@hotmail.com>
2022-10-12 14:44:45 +0200

    random doublesteen

---
+++ project.py
@@ -0,0 +1,94 @@
+import random
+from tkinter import *
+
+ 
+x = "y"
+  
+while x == "y":
+    doublesteen = random.randint(1,6)
+     
+    if doublesteen == 1:
+        print("┌─────────┐")
+        print("|         |")
+        print("|    0    |")
+        print("|         |")
+        print("└─────────┘")
+    if doublesteen == 2:
+        print("┌─────────┐")
+        print("|  0      |")
+        print("|         |")
+        print("|      0  |")
+        print("└─────────┘")
+    if doublesteen == 3:
+        print("┌─────────┐")
+        print("|  0      |")
+        print("|    0    |")
+        print("|       0 |")
+        print("└─────────┘")
+    if doublesteen == 4:
+        print("┌─────────┐")
+        print("| 0     0 |")
+        print("|         |")
+        print("| 0     0 |")
+        print("└─────────┘")
+    if doublesteen == 5:
+        print("┌─────────┐")
+        print("| 0     0 |")
+        print("|    0    |")
+        print("| 0     0 |")
+        print("└─────────┘")
+    if doublesteen == 6:
+        print("┌─────────┐")
+        print("| 0  0  0 |")
+        print("|         |")
+        print("| 0  0  0 |")
+        print("└─────────┘")
+         
+    x=input("press y to roll again and n to exit:")
+    print("\n")
+    if input == "n":
+        print("Thanks for playing")
+
+v = 'y'
+while v == "y":
+    doublesteen = random.randint(1,6)
+     
+    if doublesteen == 1:
+        print("┌─────────┐")
+        print("|         |")
+        print("|    0    |")
+        print("|         |")
+        print("└─────────┘")
+    if doublesteen == 2:
+        print("┌─────────┐")
+        print("|  0      |")
+        print("|         |")
+        print("|      0  |")
+        print("└─────────┘")
+    if doublesteen == 3:
+        print("┌─────────┐")
+        print("|  0      |")
+        print("|    0    |")
+        print("|       0 |")
+        print("└─────────┘")
+    if doublesteen == 4:
+        print("┌─────────┐")
+        print("| 0     0 |")
+        print("|         |")
+        print("| 0     0 |")
+        print("└─────────┘")
+    if doublesteen == 5:
+        print("┌─────────┐")
+        print("| 0     0 |")
+        print("|    0    |")
+        print("| 0     0 |")
+        print("└─────────┘")
+    if doublesteen == 6:
+        print("┌─────────┐")
+        print("| 0  0  0 |")
+        print("|         |")
+        print("| 0  0  0 |")
+        print("└─────────┘")
+         
+    v=input("press y to roll again and n to exit:")
+    print("\n")  
